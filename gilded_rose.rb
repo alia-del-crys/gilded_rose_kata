dda08d3b0b34c96d5c6c260d9a91043452e8b6e7
alia-del-crys <rocio.fdez.diaz.meco@gmail.com>
2015-07-04 23:50:13 +0200

    pasado por rubocop

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,83 +1,76 @@
 def update_quality(items)
-  @names_method = {'Aged Brie' => 'rule_aged' ,
-                  'Backstage passes to a TAFKAL80ETC concert' => 'rule_bakstage_passes', 
-                  'Sulfuras, Hand of Ragnaros' => 'rule_sulfuras',
-                  'NORMAL ITEM' => 'rule_normal_item', 
-                  'Conjured Mana Cake'=> 'rule_conjured'}
+  @names_method = { 'Aged Brie' => 'rule_aged',
+                    'Backstage passes to a TAFKAL80ETC concert' => 'rule_bakstage',
+                    'Sulfuras, Hand of Ragnaros' => 'rule_sulfuras',
+                    'NORMAL ITEM' => 'rule_normal_item',
+                    'Conjured Mana Cake' => 'rule_conjured' }
 
-  items.each do |item|    
+  items.each do |item|
     choose_quality(item)
   end
 end
 
 def choose_quality(item)
   if item.quality == 50
-    item.sell_in -=1
+    item.sell_in -= 1
   elsif item.quality == 0
-    item.sell_in -=1
-  elsif
+    item.sell_in -= 1
+  else
     @names_method.each do |name, method|
       if item.name == name
         send(method, item)
-        #http://stackoverflow.com/questions/13795627/ruby-send-method-passing-multiple-parameters
+        # http://stackoverflow.com/questions/13795627/ruby-send-method-passing-multiple-parameters
       end
     end
   end
 end
 
-
 def rule_normal_item(item)
-   if item.sell_in > 0
-    item.quality -=1
+  if item.sell_in > 0
+    item.quality -= 1
   else
-    item.quality -=2
+    item.quality -= 2
   end
   item.sell_in -= 1
 end
 
-def rule_sulfuras(item)
-
+def rule_sulfuras(_item)
 end
-
 
 def rule_aged(item)
-
   if item.sell_in > 0
-    item.quality +=1 unless item.quality >=50
+    item.quality += 1 unless item.quality >= 50
   else
-    if item.quality >=48
-      item.quality =50
+    if item.quality >= 48
+      item.quality = 50
     else
-      item.quality +=2 unless item.quality >=48
+      item.quality += 2 unless item.quality >= 48
     end
   end
-  
   item.sell_in -= 1
 end
 
-def rule_bakstage_passes(item)
+def rule_bakstage(item)
   if item.sell_in >= 11
-    item.quality +=1
+    item.quality += 1
   elsif item.sell_in >= 6 && item.sell_in <= 10
-    item.quality +=2
+    item.quality += 2
   elsif item.sell_in > 0 && item.sell_in <= 5
     item.quality += 3
-  else item.sell_in <= 0
+  elsif item.sell_in <= 0
     item.quality = 0
   end
-  
-    item.sell_in -= 1
+  item.sell_in -= 1
 end
 
 def rule_conjured(item)
-  if item.sell_in > 0 
-    item.quality -=2
-  else item.sell_in <=0 
-    item.quality -=4 
-  end  
+  if item.sell_in > 0
+    item.quality -= 2
+  elsif item.sell_in <= 0
+    item.quality -= 4
+  end
   item.sell_in -= 1
 end
-
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
